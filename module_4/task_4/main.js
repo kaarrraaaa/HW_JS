7b6169f4f043b124272db0f321770b601782b056
Karina Gagarina <122288632+kaarrraaaa@users.noreply.github.com>
2023-11-29 13:01:55 +0500

    Update main.js

--- module_4/task_4/main.js
+++ module_4/task_4/main.js
@@ -3,13 +3,8 @@ let arr1 = [2, 2, 17, 21, 45, 12, 54, 31, 53];
 let arr2 = [12, 44, 23, 5];
 
 // Объединение массивов
-let arrNew = [];
-for (let i = 0; i < arr1.length; i++) {
- arrNew.push(arr1[i]);
-}
-for (let i = 0; i < arr2.length; i++) {
- arrNew.push(arr2[i]);
-}
+let arrNew = arr1.concat(arr2);
 
 // Вывод результата
-console.log(arrNew);
+console.log(arrNew)
+
